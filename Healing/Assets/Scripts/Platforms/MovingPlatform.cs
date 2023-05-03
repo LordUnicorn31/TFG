6b80a56d8bdce2432f4825a31ef3b5504fe3c573
Jordi Espriu <37652360+LordUnicorn31@users.noreply.github.com>
2023-05-03 17:25:39 +0200

    moving platform is fixed when player is on it

--- Healing/Assets/Scripts/Platforms/MovingPlatform.cs
+++ Healing/Assets/Scripts/Platforms/MovingPlatform.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MovingPlatform : MonoBehaviour
 {
     private bool isMoving = false;
+    private bool isPlayer = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,9 +30,26 @@ public class MovingPlatform : MonoBehaviour
 
     private void OnMouseDrag()
     {
-        if(isMoving)
+        if(isMoving && !isPlayer)
         {
             transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        // We dont want the platform to move when the player is staying on it
+        if(collision.collider.tag == "Player")
+        {
+            isPlayer = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            isPlayer = false;
         }
     }
 }
