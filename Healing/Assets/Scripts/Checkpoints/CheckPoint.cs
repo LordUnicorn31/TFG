832fe1fd27f78220b44d30a626cc24d4cb01170a
Jordi Espriu <37652360+LordUnicorn31@users.noreply.github.com>
2023-05-22 12:43:22 +0200

    Checkpoint change background color logic created

--- Healing/Assets/Scripts/Checkpoints/CheckPoint.cs
+++ Healing/Assets/Scripts/Checkpoints/CheckPoint.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CheckPoint : MonoBehaviour
 {
     [SerializeField] Vector3 checkPointPos;
     [SerializeField]private PlayerMovement player;
     private bool enlight = true;
+    [SerializeField] GameObject background;
+    private Tilemap tilemap;
+    [SerializeField] Color backGroundColor;
 
     // Start is called before the first frame update
     void Start()
     {
         checkPointPos = transform.position;
+        tilemap = background.GetComponent<Tilemap>();
     }
 
     // Update is called once per frame
@@ -29,6 +34,7 @@ public class CheckPoint : MonoBehaviour
             if(enlight)
             {
                 player.Enlight();
+                tilemap.color = backGroundColor;
                 enlight = false;
             }
             
