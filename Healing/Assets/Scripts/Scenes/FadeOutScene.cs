b9ae88f0023f54819bba774a916f93dc115de42b
Jordi Espriu <37652360+LordUnicorn31@users.noreply.github.com>
2023-06-05 11:40:29 +0200

    Player gets instantiated in the play button instead of the scene

--- Healing/Assets/Scripts/Scenes/FadeOutScene.cs
+++ Healing/Assets/Scripts/Scenes/FadeOutScene.cs
@@ -7,6 +7,7 @@ public class FadeOutScene : MonoBehaviour
 {
     public Image fadeOutImage;
     [SerializeField] string sceneName;
+    [SerializeField] GameObject playerPrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,13 +23,31 @@ public class FadeOutScene : MonoBehaviour
     public void Wrapper()
     {
         StartCoroutine(FadeOut());
+        StopCoroutine(FadeOut());
     }
+
+    public void WrapperInstantiate()
+    {
+        StartCoroutine(FadeOutInstantiate());
+        StopCoroutine(FadeOutInstantiate());
+    }
+
     private IEnumerator FadeOut()
     {
         fadeOutImage.CrossFadeAlpha(1.0f, 3.0f, false);
 
         yield return new WaitForSeconds(3.0f);
+        
+        SceneManager.LoadScene(sceneName);
+        
+    }
 
+    private IEnumerator FadeOutInstantiate()
+    {
+        fadeOutImage.CrossFadeAlpha(1.0f, 3.0f, false);
+
+        yield return new WaitForSeconds(3.0f);
+        Instantiate(playerPrefab);
         SceneManager.LoadScene(sceneName);
     }
 
