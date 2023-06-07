6ec358e81809d7f7eb004f09961aa09f00189911
Jordi Espriu <37652360+LordUnicorn31@users.noreply.github.com>
2023-06-07 09:51:46 +0200

    player spawn set when entering a new scene

--- Healing/Assets/Scripts/Scenes/CavesScene.cs
+++ Healing/Assets/Scripts/Scenes/CavesScene.cs
@@ -16,6 +16,8 @@ public class CavesScene : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        player.position = spawn;
+
         if (player.notLoaded)
         {
             player.transform.position = spawn;
